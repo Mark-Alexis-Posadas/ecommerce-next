50be807703b9a0ef03b8d4b87b26ec931186ac1c
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2025-02-03 09:13:03 +0800

    added nav active color

--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -1,15 +1,27 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
-    <nav className="flex items-center gap-3">
-      <li>
+    <nav className="flex items-center gap-3 p-5">
+      <li
+        className={`${
+          pathname === "/" ? "text-green-500" : "text-gray-500"
+        } list-none`}
+      >
         <Link href="/">Home</Link>
       </li>
-      <li>
+      <li
+        className={`${
+          pathname === "/shop" ? "text-green-500" : "text-gray-500"
+        } list-none`}
+      >
         <Link href="/shop">Shop</Link>
       </li>
-      <button>Cart bag</button>
+      <button className="ml-auto">Cart bag</button>
     </nav>
   );
 }
