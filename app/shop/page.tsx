77a82c6dfe55e4c379592071e6b04f0bc0ddb743
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2025-02-11 07:56:26 +0800

    remove async in shop page

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,39 +1,14 @@
-import Link from "next/link";
-import ProductCard from "../components/product-card";
-import Buttons from "../components/buttons";
+import { Suspense } from "react";
+import ProductList from "../components/product-list";
+import Loading from "../loading";
 
-interface CardProps {
-  id: number;
-  image: string;
-  title: string;
-  category: string;
-  price: number;
-}
-
-export default async function Shop() {
-  const data = await fetch("https://fakestoreapi.com/products");
-  const products = await data.json();
-
+export default function Shop() {
   return (
-    <div className="p-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 max-w-[1200px] m-auto">
-      {products.map((product: CardProps) => {
-        const { id, image, title, category, price } = product;
-        return (
-          <div key={id} className="text-center group relative">
-            <Link href={`/shop/${id}`}>
-              <ProductCard
-                image={image}
-                title={title}
-                category={category}
-                price={price}
-              />
-            </Link>
-            <Buttons className="text-white bg-green-500 p-2 rounded transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 absolute left-1/2 top-1/2">
-              Add to Cart
-            </Buttons>
-          </div>
-        );
-      })}
-    </div>
+    <section className="text-center mt-10 max-w-[1200px] m-auto">
+      <h1 className="font-bold text-4xl my-3">Browse Products</h1>
+      <Suspense fallback={<Loading />}>
+        <ProductList />
+      </Suspense>
+    </section>
   );
 }
