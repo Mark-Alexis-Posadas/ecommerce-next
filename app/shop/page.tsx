ffddd9473973184d3672ab74e87fc0925b8b46b5
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2025-02-14 19:36:43 +0800

    optimiz shop page

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,17 +1,12 @@
 import { Suspense } from "react";
 
-import ProductList from "../components/product-list";
-import Loading from "../loading";
+import Category from "../components/category";
 
-export default async function Shop() {
+export default function Shop() {
   return (
     <section className="text-center mt-10 max-w-[1200px] m-auto">
       <h1 className="font-bold text-4xl my-3">Browse Products</h1>
-      <div className="flex">
-        <Suspense fallback={<Loading />}>
-          <ProductList />
-        </Suspense>
-      </div>
+      <Category />
     </section>
   );
 }
